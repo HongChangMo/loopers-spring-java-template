f90a0eb3e687ddbb495eaba6a2ab0083d0bc01e0
HongChangMo <hcm7402@naver.com>
2025-12-11 23:03:18 +0900

    refactor: @EnableFeignClients 어노테이션 선언 위치 변경

--- apps/commerce-api/src/main/java/com/loopers/config/FeignClientConfig.java
+++ apps/commerce-api/src/main/java/com/loopers/config/FeignClientConfig.java
@@ -4,11 +4,9 @@ import feign.Logger;
 import feign.Request;
 import feign.RequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableFeignClients
 @Configuration
 public class FeignClientConfig {
 
