d5068dd16db918f9b3426ef47a2e8248c64dd216
HongChangMo <hcm7402@naver.com>
2026-01-02 15:55:03 +0900

    feat: 주간, 월간 랭킹 제공 api 구현
    - PeriodType별 일간, 주간, 월간 랭킹 제공
    - 일간은 Redis ZSET 으로 제공
    - 주간, 월간 MV를 활용한 랭킹 제공

--- apps/commerce-api/src/main/java/com/loopers/domain/ranking/Ranking.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/ranking/Ranking.java
@@ -8,11 +8,42 @@ public class Ranking {
     private final int rank;
     private final Long productId;
     private final Double score;
+    private final Long totalLikeCount;
+    private final Long totalViewCount;
+    private final Long totalOrderCoun;
 
     @Builder
-    public Ranking(int rank, Long productId, Double score) {
+    public Ranking(
+            int rank,
+            Long productId,
+            Double score,
+            Long totalLikeCount,
+            Long totalViewCount,
+            Long totalOrderCoun
+    ) {
         this.rank = rank;
         this.productId = productId;
         this.score = score;
+        this.totalLikeCount = totalLikeCount;
+        this.totalViewCount = totalViewCount;
+        this.totalOrderCoun = totalOrderCoun;
+    }
+
+    public static Ranking of(
+            int i,
+            Long productId,
+            double score,
+            Long totalLikeCount,
+            Long totalViewCount,
+            Long totalOrderCount
+    ) {
+        return new  Ranking(
+                i,
+                productId,
+                score,
+                totalLikeCount,
+                totalViewCount,
+                totalOrderCount
+        );
     }
 }
